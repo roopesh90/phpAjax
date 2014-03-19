c56dd59694c87233f566b6b59dc310acd660cdc9
roopesh90 <roopesh90nair@live.com>
2014-03-19 19:15:35 +0530

    loading animation added ajax send and open shited to on change funtion 800ms timeout added to send

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 		
 </form>
 
-<p id="loading">Loading...</p>
+<p id="loading" class="">Loading...</p>
 
 <script>
 	var reqListener = function(){
@@ -35,6 +35,7 @@
 		result = onReq.responseText;
 		console.log(onReq.responseText);
 		
+		loading();
 	}
 	
 	var onReq = new XMLHttpRequest();
@@ -47,22 +48,69 @@
 	 *with the help of '&'
 	 *Refer: http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp
 	 */
-	onReq.open("POST", "get_data.php", true);
-	onReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	
 	var companyChanged = function(company){
+		loading();
 		document.forms.sample.ass1.value=""
 		document.forms.sample.ass2.value=""
 		
 		if(company != "init")
 		{
+			onReq.open("POST", "get_data.php", true);
+			onReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	
 			var user_id = <?php echo "'$user_id'"?>;
-			alert(company);
-			onReq.send("id="+user_id+"&company="+company);
+			setTimeout(	function(){
+				onReq.send("id="+user_id+"&company="+company);
+				}, 800);
 		}
 	}
 	
 	var loading = function(){
-		
+		var anim_elem = document.getElementById('loading');
+		anim_elem.className = anim_elem.className == ""? "activate" : "";
 	}
 </script>
+
+<style>
+	#loading{
+		font-size: 24px;
+		font-weight: bold;
+		opacity: 0;
+	}
+	
+	.activate {
+		-webkit-animation-name: blinker;
+		-webkit-animation-duration: 0.5s;
+		-webkit-animation-timing-function: linear;
+		-webkit-animation-iteration-count: infinite;
+		
+		-moz-animation-name: blinker;
+		-moz-animation-duration: 1s;
+		-moz-animation-timing-function: linear;
+		-moz-animation-iteration-count: infinite;
+		
+		animation-name: blinker;
+		animation-duration: 1s;
+		animation-timing-function: linear;
+		animation-iteration-count: infinite;
+	}
+
+	@-moz-keyframes blinker {  
+		0% { opacity: 1.0; }
+		50% { opacity: 0.0; }
+		100% { opacity: 1.0; }
+	}
+
+	@-webkit-keyframes blinker {  
+		0% { opacity: 1.0; }
+		50% { opacity: 0.0; }
+		100% { opacity: 1.0; }
+	}
+
+	@keyframes blinker {  
+		0% { opacity: 1.0; }
+		50% { opacity: 0.0; }
+		100% { opacity: 1.0; }
+	}
+</style>
